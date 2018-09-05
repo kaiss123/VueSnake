69465b9579d1c432468cf438482f9f23c8e11d73
kize <haririkaiss@gmx.de>
2018-09-05 08:49:20 +0200

    merge

--- src/components/Snake.vue
+++ src/components/Snake.vue
@@ -1,32 +1,61 @@
 <template>
   <div class="game">
-
+    <div v-for="column in gridSize" class="grid-column">
+      <div v-for="row in column" class="grid-row" v-bind:class="getCellValue(row, column)">
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Snake',
-  props: {
-    msg: String
-  }
+    props: {
+        gridSize: 32,
+    },
+    created: {
+},
+methods: {
+    getCellValue(r, c) {
+        let cellValue;
+        if (r === 2 && c === 3) {
+            cellValue = 'snake1';
+        }
+        return cellValue;
+    },
+    handleKeydown(e) {
+        console.log(e);
+    },
+
+},
+mounted() {
+    window.addEventListener('keydown', e => {
+        console.log(e);
+    });
+},
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  .game {
+    height: 704px;
+    width: 704px;
+    display: flex;
+    border: 2px solid black;
+  }
+
+  .grid-column {
+  }
+
+  .grid-row {
+    width: 20px;
+    height: 20px;
+    border: 1px solid #aacccc;
+    background-color: #cccccc;
+  }
+
+  .snake {
+    background-color: red;
+  }
 </style>
